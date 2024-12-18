f5b2d28f988ec2ee35c555f1c0591560dee31d6e
deenafink <deena.fink03@gmail.com>
2024-12-17 21:35:00 -0500

    now did it without sprintf and added multiple vars

--- startSimpler.c
+++ startSimpler.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdarg.h>
 // plan is to use putc() or putchar() to take a char at a time and put them into a list which will ultimately be printed
 // loop through the first parameter and get each char into an array
 // put them to stdout
@@ -9,16 +10,39 @@
 // next step: if encounter % then go to a section that asks if is %d, %x, %c, or %s and do different things based on that
 // if %d then need to add to the array as a decimal
 
-void my_printf(char *phrase, int nums) {
+void my_printf(char *phrase, ...) {
+    va_list ap; // initialize list for variables
+    va_start(ap, phrase);
     while (*phrase != '\0') {
         if (*phrase == '%') {
             // move to the next character to get type
             phrase++;
             if (*phrase == 'd') {
-                char str[12]; // later want to turn the int into a str
-                sprintf(str, "%d", nums); // int to string
-                // now need to putchar() the chars
-                for (int i = 0; str[i] != '\0'; i++) {
+                int num = va_arg(ap, int); // get the next argument
+                // cast it into an int
+                int ascii = (int) num;
+                
+                // sizes can only be 10 places anyway
+                char str[12]; // later want to turn the int into a str to print each digit individually
+                int index = 0;
+                
+                // turn the into a string, but backwards
+                while (ascii > 0) {
+                    int temp = ascii % 10;
+                    //printf("%d", temp);
+                    // add this digit to the string
+                    str[index] = '0' + temp;
+                    index++;
+                    // get rid of last digit
+                    ascii /= 10;
+                }
+                // add a teminal char
+                //str[index] = '\0';
+                //index++;
+                
+                // now need to putchar() the chars - backwards
+                for (int i = index-1; i >= 0; i--) {
+                    //printf("here");
                     putchar(str[i]);
                 }
             }
@@ -28,6 +52,7 @@ void my_printf(char *phrase, int nums) {
         }
         phrase++;
     }
+    va_end(ap); // cleanup the va_list
 }
 
 int main() {
@@ -36,6 +61,14 @@ int main() {
 
     printf("Fake printf(): ");
     my_printf("Hello World %d\n", 82);
+
+    printf("Normal printf(): ");
+    printf("Hello World %d\n", 'f');
+
+    printf("Fake printf(): ");
+    my_printf("Hello World %d\n", 'f');
+
+
 
     return 0;
 }
